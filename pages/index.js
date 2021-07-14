a3583e295640e5be68a48bb98de100d89b16897d
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2021-07-14 13:26:41 +0200

    tried out some client side fetching, learning more

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
+export default function Home({ data }) {
+  const [joke, setJoke] = useState({});
 
-export default function Home() {
+  const fetchJoke = async () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setJoke(data);
+      });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetchJoke();
+  };
+  useEffect(() => {
+    fetchJoke();
+  }, []);
   return (
     <div>
       <Head>
@@ -9,6 +26,13 @@ export default function Home() {
       </Head>
       <h1>Manuel Puchner portfolio</h1>
       <p>please note this is currently in development</p>
+
+      <br />
+      <br />
+      <button onClick={handleClick}>test</button>
+      <div className="joke">
+        <h1>{joke.value}</h1>
+      </div>
     </div>
-  )
+  );
 }
