c4c944cbfa8aaa4e28db2e8de1b4db05a28868b2
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2021-07-17 14:11:28 +0200

    background now dynamically changes height

--- components/background/Background.js
+++ components/background/Background.js
@@ -1,8 +1,10 @@
+import { useState, useEffect } from "react";
+
 import styles from "./Background.module.scss";
 import shapestyles from "./BackgroundShape.module.scss";
 import BackgroundShape from "./BackgroundShape";
 
-function background() {
+function Background() {
   const shapecolors = [
     "rgb(36, 35, 41)",
     "rgb(70, 69, 74)",
@@ -11,9 +13,17 @@ function background() {
     "rgb(245, 245, 255)",
   ];
 
-  
+  const [height, setHeight] = useState(0);
+  useEffect(() => {
+    setHeight(document.documentElement.clientHeight);
+    const resizeObserver = new ResizeObserver((entries) =>
+      setHeight(document.documentElement.clientHeight)
+    );
+    resizeObserver.observe(document.body);
+  }, []);
+
   return (
-    <div className={styles.bgWrapper}>
+    <div className={styles.bgWrapper} style={{ height: height }}>
       <div className={styles.bg}>
         {/* big shapes */}
         <BackgroundShape id={shapestyles._1} />
@@ -32,4 +42,4 @@ function background() {
   );
 }
 
-export default background;
+export default Background;
